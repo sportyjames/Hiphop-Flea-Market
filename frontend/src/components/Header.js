2339539744fdef065c8d33a78188f5d8626f4b03
James <sportyjames2000@gmail.com>
2021-07-26 21:33:49 -0500

    React get Started

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Hiphop Flea Market</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>
+                  Login
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
